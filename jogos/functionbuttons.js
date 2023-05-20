8ea2af6f5a978c68031f884b816d8ab50646ce4d
Rodrigo <119820531+Oozaru341@users.noreply.github.com>
2023-05-20 13:31:00 -0300

    Add files via upload

--- jogos/functionbuttons.js
+++ jogos/functionbuttons.js
@@ -43,35 +43,16 @@ document.getElementById("reset-zoom").addEventListener("click", function(){
     document.getElementById("reset-zoom").classList.add("disabled");
 });
 
- function toggleFullScreen() {
-            var elem = document.getElementById("myElement");
-
-            if (elem.requestFullscreen) {
-                if (document.fullscreenElement) {
-                    document.exitFullscreen();
-                } else {
-                    elem.requestFullscreen();
-                }
-            } else if (elem.mozRequestFullScreen) { // Firefox
-                if (document.mozFullScreenElement) {
-                    document.mozCancelFullScreen();
-                } else {
-                    elem.mozRequestFullScreen();
-                }
-            } else if (elem.webkitRequestFullscreen) { // Chrome, Safari and Opera
-                if (document.webkitFullscreenElement) {
-                    document.webkitExitFullscreen();
-                } else {
-                    elem.webkitRequestFullscreen();
-                }
-            } else if (elem.msRequestFullscreen) { // IE/Edge
-                if (document.msFullscreenElement) {
-                    document.msExitFullscreen();
-                } else {
-                    elem.msRequestFullscreen();
-                }
-            }
-        }
+var fullscreenBtn = document.getElementById("fullscreen-btn");
+fullscreenBtn.addEventListener("click", toggleFullscreen);
+function toggleFullscreen() {
+  var doc = document.documentElement;
+  if (!document.fullscreenElement) {
+    doc.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+}
 setTimeout(function() {
         document.getElementById("loadingimg").src = "../pageelements/error.gif";
         document.getElementById("loadingtxt").innerHTML = "Falha ao carregar dados";
@@ -80,8 +61,3 @@ setTimeout(function() {
         document.getElementById("loadingimg1").src = "../pageelements/error.gif";
         document.getElementById("loadingtxt1").innerHTML = "Falha ao carregar dados";
       }, 10000); // 10 segundos em milissegundos
-	  
-	  
-	  
-	  
-	  
