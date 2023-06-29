a05dc185a5cb9d8b5ce0ce92c25aeab512d01388
Rodrigo <119820531+Oozaru341@users.noreply.github.com>
2023-06-29 12:57:53 -0300

    Add files via upload

--- jogos/ruffle.js
+++ jogos/ruffle.js
@@ -2,4 +2,9 @@ var script = document.createElement('script');
 script.src = 'https://unpkg.com/@ruffle-rs/ruffle';
 document.head.appendChild(script);
 
+window.RufflePlayer = window.RufflePlayer || {};
+window.RufflePlayer.config = {
+"forceScale": true,
+};
+
 
