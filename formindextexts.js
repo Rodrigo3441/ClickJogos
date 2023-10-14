5d9a6dc9f92db50567958eb0b9caf7dd51d5b3e8
Rodrigo <119820531+Oozaru341@users.noreply.github.com>
2023-10-14 18:31:52 -0300

    Add files via upload

--- formindextexts.js
+++ formindextexts.js
@@ -16,17 +16,3 @@ document.getElementById("contact_message").oninvalid = function(e) {
     e.target.setCustomValidity("Coloque sua mensagem");
   }
 };
-
-document.addEventListener("DOMContentLoaded", function() {
-  const form = document.getElementById("Nova_mensagem");
-  const enviarBotao = document.getElementById("enviarBotao");
-
-  form.addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita o envio padrão do formulário
-
-    // Aqui você pode adicionar código para enviar os dados do formulário para o servidor, se necessário
-
-    // Redireciona para a página desejada
-    window.location.href = "index.html";
-  });
-});
