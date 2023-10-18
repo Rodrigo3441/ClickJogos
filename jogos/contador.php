774294323ff93435343e3097030f422064c39750
Rodrigo <119820531+Oozaru341@users.noreply.github.com>
2023-10-18 20:29:32 -0300

    Delete jogos/contador.php

--- jogos/contador.php
+++
@@ -1,7 +0,0 @@
-<?php
-$directory = 'caminho/para/sua/pasta'; // Substitua pelo caminho da pasta onde estão os arquivos HTML
-$files = glob($directory . '/*.html');
-$numPages = count($files);
-
-echo "Total de páginas HTML na pasta: " . $numPages;
-?>
