373e15658b8522ae90c0922acc6e8e753b4abb68
Rodrigo <119820531+Oozaru341@users.noreply.github.com>
2023-10-18 21:09:49 -0300

    Add files via upload

---
+++ gamescounter.js
@@ -0,0 +1,28 @@
+ // Token de acesso pessoal
+const token = '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';
+
+// Nome do usuário e nome do repositório
+const username = 'Oozaru341';
+const repo = 'ClickJogos';
+const subpasta = 'jogos';
+
+ // Função para contar as páginas HTML na subpasta
+        function contarPaginas() {
+            fetch(`https://api.github.com/repos/${username}/${repo}/contents/${subpasta}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`, // Inclua o token no cabeçalho
+                    Accept: 'application/vnd.github.v3+json'
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                const paginasHTML = data.filter(file => file.name.endsWith('.html'));
+                document.getElementById("numero").textContent = paginasHTML.length;
+            })
+            .catch(error => {
+                console.error(error);
+                document.getElementById("numero").textContent = "Erro na contagem de páginas";
+            });
+        }
+
+        contarPaginas(); // Chama a função para contar as páginas
