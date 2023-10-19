c58f4446c95f57e64b54ab32e0feae25872cdfbd
Rodrigo <119820531+Oozaru341@users.noreply.github.com>
2023-10-18 21:46:03 -0300

    Update gamescounter.js

--- gamescounter.js
+++ gamescounter.js
@@ -1,5 +1,5 @@
  // Token de acesso pessoal
-const token = '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';
+const token = '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';
 
 // Nome do usuário e nome do repositório
 const username = 'Oozaru341';
