7c53c4ee9959a96184db708a90ede6df1789747e
Rodrigo <119820531+Oozaru341@users.noreply.github.com>
2023-10-18 21:36:23 -0300

    Add files via upload

--- gamescounter.js
+++ gamescounter.js
@@ -1,5 +1,5 @@
  // Token de acesso pessoal
-const token = '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';
+const token = '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';
 
 // Nome do usuário e nome do repositório
 const username = 'Oozaru341';
