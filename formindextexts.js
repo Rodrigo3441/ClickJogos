67c454c3c4e3ec1309f6872b4564a69a7d18a7b6
Rodrigo <119820531+Oozaru341@users.noreply.github.com>
2023-10-22 19:39:23 -0300

    Add files via upload

--- formindextexts.js
+++ formindextexts.js
@@ -16,3 +16,21 @@ document.getElementById("contact_message").oninvalid = function(e) {
     e.target.setCustomValidity("Coloque sua mensagem");
   }
 };
+
+function mostrarAlerta(){
+	window.alert("Mensagem enviada com sucesso!");
+}
+
+document.getElementById("Nova_mensagem").addEventListener("submit", function(event) {
+       
+            event.preventDefault();
+
+           
+            if (document.getElementById("Nova_mensagem").checkValidity()) {
+                mostrarAlerta();
+				document.getElementById("Nova_mensagem").style.display = "none";
+				document.getElementById("completed").style.display = "block";
+            } else {
+                window.alert("Preencha todos os campos corretamente antes de enviar.");
+            }
+        });
