d8005cc3cde42ed262474755dbbe9c34f6d45834
Rodrigo <119820531+Oozaru341@users.noreply.github.com>
2023-11-23 18:57:29 -0300

    Add files via upload

--- formindextexts.js
+++ formindextexts.js
@@ -20,23 +20,7 @@ document.getElementById("contact_message").oninvalid = function(e) {
 document.getElementById("email").oninvalid = function(e) {
   e.target.setCustomValidity("");
   if (!e.target.validity.valid) {
-    e.target.setCustomValidity("Coloque seu E-Mail");
+    e.target.setCustomValidity("Coloque seu E-Mail terminado em @gmail.com, @hotmail.com ou @outlook.com");
   }
 };
 
-//validar email
-
- function validarEmail() {
-            var emailInput = document.getElementById("email");
-            var email = emailInput.value;
-            var regex = /@(gmail\.com|hotmail\.com|outlook\.com)$/;
-
-            if (!regex.test(email)) {
-                alert("Por favor, insira um e-mail válido do tipo: @gmail.com, @hotmail.com ou @outlook.com.");
-                // Limpar o campo de e-mail se for inválido
-                emailInput.value = "";
-                return false; // Impede o envio do formulário
-            }
-
-            return true; // Permite o envio do formulário se o e-mail for válido
-        }
