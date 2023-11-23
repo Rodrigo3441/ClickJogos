b17adccdca5cc582b424f15091ef4773a5665948
Rodrigo <119820531+Oozaru341@users.noreply.github.com>
2023-11-22 21:11:19 -0300

    Add files via upload

--- formindextexts.js
+++ formindextexts.js
@@ -17,6 +17,26 @@ document.getElementById("contact_message").oninvalid = function(e) {
   }
 };
 
-function mostrarAlerta(){
-	window.alert("Mensagem enviada com sucesso!");
-}
+document.getElementById("email").oninvalid = function(e) {
+  e.target.setCustomValidity("");
+  if (!e.target.validity.valid) {
+    e.target.setCustomValidity("Coloque seu E-Mail");
+  }
+};
+
+//validar email
+
+ function validarEmail() {
+            var emailInput = document.getElementById("email");
+            var email = emailInput.value;
+            var regex = /@(gmail\.com|hotmail\.com|outlook\.com)$/;
+
+            if (!regex.test(email)) {
+                alert("Por favor, insira um e-mail válido do tipo: @gmail.com, @hotmail.com ou @outlook.com.");
+                // Limpar o campo de e-mail se for inválido
+                emailInput.value = "";
+                return false; // Impede o envio do formulário
+            }
+
+            return true; // Permite o envio do formulário se o e-mail for válido
+        }
